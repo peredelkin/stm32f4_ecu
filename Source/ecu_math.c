b2228c72a2666b827f39069451a9d6fa7f4c38a3
Ruslan <ep8kha@gmail.com>
2018-12-13 21:45:59 +0500

    оптимизация вычислений

--- Source/ecu_math.c
+++ Source/ecu_math.c
@@ -18,33 +18,26 @@ void crank_extrapolation_period_calc(ecu_t* ecu, uint8_t next_numb, uint16_t per
     ecu->crank.period[next_numb] = (period * (next_period_angles / 2)) / (period_angles / 2); // "/2" - проблемы с математикой
 }
 
-void crank_extrapolation_next_1_period(ecu_t* ecu,uint8_t vr_count) {
-    uint8_t prev_1 = ecu_crank_vr_numb_normalization(vr_count - 1);
-    uint8_t next_1 = ecu_crank_vr_numb_normalization(vr_count + 1);
+void crank_extrapolation_next_1_period(ecu_t* ecu,uint8_t prev_1,uint8_t vr_count,uint8_t next_1) {
     //расчет текущего периода
     crank_extrapolation_period_calc(ecu, next_1, ecu->crank.period[vr_count],
             ecu->crank.angle[vr_count] - ecu->crank.angle[prev_1],
             ecu->crank.angle[next_1] - ecu->crank.angle[vr_count]);
 }
 
-void crank_extrapolation_next_1_capture(ecu_t* ecu,uint8_t vr_count) {
-    uint8_t next_1 = ecu_crank_vr_numb_normalization(vr_count + 1);
+void crank_extrapolation_next_1_capture(ecu_t* ecu,uint8_t vr_count,uint8_t next_1) {
     //расчет следующего захвата
     ecu->crank.capture[next_1] = ecu->crank.capture[vr_count] + ecu->crank.period[next_1];
 }
 
-void crank_extrapolation_next_2_period(ecu_t* ecu,uint8_t vr_count) {
-    uint8_t next_1 = ecu_crank_vr_numb_normalization(vr_count + 1);
-    uint8_t next_2 = ecu_crank_vr_numb_normalization(vr_count + 2);
+void crank_extrapolation_next_2_period(ecu_t* ecu,uint8_t vr_count,uint8_t next_1,uint8_t next_2) {
     //расчет следующего периода
     crank_extrapolation_period_calc(ecu, next_2, ecu->crank.period[next_1],
             ecu->crank.angle[next_1] - ecu->crank.angle[vr_count],
             ecu->crank.angle[next_2] - ecu->crank.angle[next_1]);
 }
 
-void crank_extrapolation_next_2_capture(ecu_t* ecu,uint8_t vr_count) {
-    uint8_t next_1 = ecu_crank_vr_numb_normalization(vr_count + 1);
-    uint8_t next_2 = ecu_crank_vr_numb_normalization(vr_count + 2);
+void crank_extrapolation_next_2_capture(ecu_t* ecu,uint8_t next_1,uint8_t next_2) {
     //расчет захвата следующего периода
     ecu->crank.capture[next_2] = ecu->crank.capture[next_1] + ecu->crank.period[next_2];
 }
@@ -53,9 +46,12 @@ void crank_extrapolation_next_2_capture(ecu_t* ecu,uint8_t vr_count) {
 void crank_extrapolation_calc(ecu_t *ecu) {
     if (ecu->gap_found) {
         uint8_t vr_count = ecu->vr_count;
-        crank_extrapolation_next_1_period(ecu,vr_count);
-        crank_extrapolation_next_1_capture(ecu,vr_count);
-        crank_extrapolation_next_2_period(ecu,vr_count);
+        uint8_t prev_1 = ecu_crank_vr_numb_normalization(vr_count - 1); //получение предыдущего номера
+        uint8_t next_1 = ecu_crank_vr_numb_normalization(vr_count + 1); //получение следующего номера
+        uint8_t next_2 = ecu_crank_vr_numb_normalization(vr_count + 2); //получение следующего за следующим номером
+        crank_extrapolation_next_1_period(ecu,prev_1,vr_count,next_1); //период next_1
+        crank_extrapolation_next_1_capture(ecu,vr_count,next_1); //захват next_1
+        crank_extrapolation_next_2_period(ecu,vr_count,next_1,next_2); //период next_2
     }
 }
 
